65feab5d8ca89f2a17e5ee5cffdf542076ee7230
nonaaaaaaaaa <nehalserag44@gmail.com>
2024-05-07 05:21:40 -0700

    l

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,22 +6,20 @@ of the first 10 hot posts listed for a given subreddit.
 
 import requests
 
-
 def top_ten(subreddit):
-    """
-    Function that queries the Reddit API
-    - If not a valid subreddit, print None.
-    """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-        params={"limit": 10},
-    )
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'Custom User Agent'}  # Add a custom user agent to prevent Too Many Requests error
 
-    if req.status_code == 200:
-        for get_data in req.json().get("data").get("children"):
-            dat = get_data.get("data")
-            title = dat.get("title")
-            print(title)
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts:
+            print(post['data']['title'])
     else:
-        print(None)
+        print("None")
+
+subreddit = 'learnprogramming'
+print(f"Top 10 hot posts in r/{subreddit}:")
+top_ten(subreddit)
